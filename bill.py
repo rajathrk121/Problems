52b4cd1f11e03d9874b32940dabdba71cbe106c7
Rajath Krishna M <rkcutz2345@gmail.com>
2026-09-16 13:42:19 +0530

    new

--- bill.py
+++ bill.py
@@ -4,9 +4,9 @@ if unit<0 :
     print("Enter a valid number")
 elif unit <=100:
     price=unit*5
-elif unit<=200:
+elif unit<=2000:
     price=unit*7
 else:
-    price=unit*10
+    price=unit*100
 
 print(price)
